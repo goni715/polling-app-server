128f5db2aee98973c93030fb5bc682263b8aceeb
goni715 <halesgoni715149123@gmail.com>
2025-02-06 22:32:49 +0600

    middleware implementation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import express, {Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response)=> {
-    res.send('This is Express Pollig Application Server');
-})
 
+//middleware implementation
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("This is Express Pollig Application Server");
+  });
 
 export default app;
