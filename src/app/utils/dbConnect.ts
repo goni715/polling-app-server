33cd30fbae95cbe84441d01f3971ef4a73f1a71a
goni715 <halesgoni715149123@gmail.com>
2025-02-07 10:26:55 +0600

    dbConnect utils

--- src/app/utils/dbConnect.ts
+++ src/app/utils/dbConnect.ts
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
+import config from '../config';
 
 const dbConnect = async() => {
-  await mongoose.connect(config.database_url)
+    try{
+        await mongoose.connect(config.database_url as string);
+        console.log('Database Connection success');
+    }catch(err){
+        console.log('Database connection failled');
+        console.log(err);
+    }
 }
+
+export default dbConnect;
