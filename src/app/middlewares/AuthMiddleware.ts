c32bd50ebe0382f468239a0b22a42f23284e455f
Osman Goni <94566411+goni715@users.noreply.github.com>
2025-02-07 21:07:23 +0600

    Update AuthMiddleware.ts

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -4,9 +4,12 @@ import { NextFunction, Request, Response } from "express";
 const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
     if(!token){
-      res.status(401).json({
+      return res.status(401).json({
           success: false,
-          message: "You are not authorized"
+          message: "You are not authorized",
+          error: {
+              message: "token must be provided"
+          }
       })
     }
 
