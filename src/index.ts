45c8f3ac59b80c1246418dbbe2b43ea8315aaab9
Osman Goni <94566411+goni715@users.noreply.github.com>
2025-02-07 10:56:07 +0600

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import config from "./app/config";
+import dbConnect from "./app/utils/dbConnect";
 
 const port = config.port || 5000;
 
@@ -7,3 +8,41 @@ const port = config.port || 5000;
 app.listen(port, ()=> {
     console.log(`Server running at @${port}`);
 })
+
+let server: Server;
+
+
+async function main() {
+    try {
+      await dbConnect();
+      server = app.listen(config.port, () => {
+        console.log(`Server running on port ${config.port}`);
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  main();
+
+
+
+  //asynchronous code error
+  process.on('unhandledRejection', (err)=>{
+    console.log(`❤❤ unahandledRejection is detected , shutting down ...`, err);
+    if(server){
+      server.close(()=>{
+        process.exit(1);
+      })
+    }
+    process.exit(1)
+  })
+
+
+
+  //synchronous code error--process immediately off
+  process.on('uncaughtException', () => {
+    console.log(`😛😛 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
+  });
