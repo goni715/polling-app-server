1ca2da994fdb07d211194278c88b75d2bd9b7ffb
goni715 <halesgoni715149123@gmail.com>
2025-02-07 20:07:07 +0600

    create user-schema

--- src/app/models/UserModel.ts
+++ src/app/models/UserModel.ts
@@ -1,1 +1,29 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
 
+
+const UserSchema = new Schema<IUser>({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+
+
+const UserModel = model<IUser>('users', UserSchema);
+export default UserModel;
