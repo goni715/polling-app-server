13c8892b4ed2c18526c0bc15cb9c679cd7ae796a
goni715 <halesgoni715149123@gmail.com>
2025-02-07 20:56:59 +0600

    create AuthMiddleware

---
+++ src/app/middlewares/AuthMiddleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+
+const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if(!token){
+      res.status(401).json({
+          success: false,
+          message: "You are not authorized"
+      })
+    }
+
+    
+    try{
+   
+
+    }
+    catch(err){
+
+    }
+}
+
+
+
+export default AuthMiddleware;
