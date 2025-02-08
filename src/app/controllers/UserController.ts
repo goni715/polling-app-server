d29ecf7460d59f290f84c2daa7c86938522143c7
goni715 <halesgoni715149123@gmail.com>
2025-02-08 12:05:13 +0600

    create user-service

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,8 +1,16 @@
 import { RequestHandler } from "express";
+import UserCreateService from "../services/user/UserCreateService";
 
-export const RegisterUser: RequestHandler = (req, res) => {
+export const RegisterUser: RequestHandler = async (req, res) => {
     try{
-      
+
+        const result = await UserCreateService(res, req.body);
+
+        res.status(201).json({
+            success: true,
+            message: "User is registered successfully",
+            data: result
+        })
     }
     catch(err:any){
         res.status(500).json({
---
+++ src/app/services/user/UserCreateService.ts
@@ -0,0 +1,8 @@
+import { Response } from "express";
+
+const UserCreateService = async (res: Response, payload) => {
+    
+}
+
+
+export default UserCreateService;
