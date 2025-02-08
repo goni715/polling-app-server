988c5f752b429c0d1a5e7839ec079302e786afe9
goni715 <halesgoni715149123@gmail.com>
2025-02-08 11:39:20 +0600

    AuthMiddleware

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -2,28 +2,54 @@ import { NextFunction, Request, Response } from "express";
 import verifyToken from "../utils/verifyToken";
 import { Secret } from "jsonwebtoken";
 import config from "../config";
+import UserModel from "../models/UserModel";
 
-const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const AuthMiddleware = async (req: Request & {user?:any}, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({
       success: false,
       message: "You are not authorized",
       error: {
-        message: 'Token must be provided'
-      }
+        message: "Token must be provided",
+      },
     });
   }
 
-   //token-verify
-   const decoded = verifyToken(
-    token,
-    config.jwt_access_secret as Secret
-  );
-
-
   try {
-  } catch (err) {}
+    //token-verify
+    const decoded = verifyToken(token, config.jwt_access_secret as Secret);
+    const {id, email, iat} = decoded;
+
+    //check if the user is exist
+    const userExist = await UserModel.findById(id);
+    if(!userExist){
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorized",
+        error: {
+          message: "This User doesn't exist",
+        },
+      })
+    }
+
+
+    req.user = decoded;
+    //set id & email to headers
+    req.headers.email= email;
+    req.headers.id= id;
+
+    next();
+
+  } catch (err:any) {
+    res.status(401).json({
+      success: false,
+      message: "You are not authorized",
+      error: {
+        message: err.message,
+      },
+    });
+  }
 };
 
 export default AuthMiddleware;
