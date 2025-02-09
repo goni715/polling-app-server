9f3cddaaee26293906d08b8490db5f9c0730c4dd
Osman Goni <94566411+goni715@users.noreply.github.com>
2025-02-09 17:11:13 +0600

    check email & username

--- src/app/services/user/UserLoginService.ts
+++ src/app/services/user/UserLoginService.ts
@@ -1,7 +1,32 @@
 import { TLoginUser } from "../../interfaces/user.interface";
 import { Response } from 'express';
+import validator from 'validator';
+import UserModel from '../../models/UserModel';
+
 const UserLoginService = async (res: Response, payload: TLoginUser) => {
-   
+   const { emailUsername, password } = payload;
+
+   //if emaiUsername = email
+   if(validator.isEmail(emailUsername)){
+      //check if email does not exist 
+      const userExists= await UserModel.findOne({email:emailUsername});
+      if(!userExists){
+         return res.status(404).json({
+            success: false,
+            message: `Couldn't find this email address`
+         })
+      }
+   }else{
+      //if emailUsername = username 
+      //check if username does not exist 
+      const userExists = await UserModel.findOne({username:emailUsername});
+      if(!userExists){
+         return res.status(404).json({
+            success: false,
+            message: `Wrong Username`
+         })
+      }  
+   }
 }
 
 export default UserLoginService;
